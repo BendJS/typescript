f87dc6c7470d34349ed47970b68ac8be9fe6d582
Andrew Haupt <ajhaupt7@gmail.com>
2017-12-12 09:21:11 -0800

    Update 2017-12-12T17:20:59.498Z

--- gulp/tasks/build.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-// Necessary Plugins
-var gulp = require('gulp');
-
-// Default task
-module.exports = gulp.task('build', ['js', 'jade', 'sass', 'imagemin']);
--- gulp/tasks/default.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// Necessary Plugins
-var gulp = require('gulp');
-
-// Default task
-module.exports = gulp.task('default', ['js', 'jade', 'sass', 'imagemin',
-  'watch', 'browser-sync']);
